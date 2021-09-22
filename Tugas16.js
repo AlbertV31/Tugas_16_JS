3e56d3abf690b9fd2b0e7f85129cb3a2e98bff89
Albert <albert.bagil@gmail.com>
2021-09-22 17:13:52 +0700

    Tugas 16 Javascript
    Tugas 16 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas16.js
@@ -0,0 +1,8 @@
+function minmax() {
+  var nilaimin = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  var nilaimax = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+  console.log("Nilai Minimal :", nilaimin);
+  console.log("Nilai Maksimal :", nilaimax);
+}
+
+minmax()
